541ad978248d0b7b11ba9836684e33374ea7403d
yelishgiri <118233081+yelishgiri@users.noreply.github.com>
2025-08-15 17:57:32 -0500

    Add The Create Routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -9,6 +9,8 @@ indexRouter.get('/product/id/:id', indexController.fetchProductInfo);
 indexRouter.get('/product/id/:id/updateProduct', indexController.updateProductGet);
 indexRouter.post('/product/id/:id/updateProduct', indexController.updateProductPost);
 indexRouter.post('/product/id/:id/deleteProduct', indexController.deleteProduct);
+indexRouter.get('/product/addProduct', indexController.addAProductGet);
+indexRouter.post('/product/addProduct', indexController.addAProductPost);
 
 
 // category routes
@@ -16,6 +18,8 @@ indexRouter.get('/category/id/:id', indexController.fetchCategoryInfo);
 indexRouter.get('/category/id/:id/updateCategory', indexController.updateCategoryGet);
 indexRouter.post('/category/id/:id/updateCategory', indexController.updateCategoryPost);
 indexRouter.post('/category/id/:id/deleteCategory', indexController.deleteCategory);
+indexRouter.get('/category/addCategory', indexController.addACategoryGet);
+indexRouter.post('/category/addCategory', indexController.addACategoryPost);
 
 
 module.exports = indexRouter;
