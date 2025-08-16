e8fdb5fddcddae700a8c3d105ea812dcfd81ce61
yelishgiri <118233081+yelishgiri@users.noreply.github.com>
2025-08-15 20:53:05 -0500

    Add Changes To The Styling

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
